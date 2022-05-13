ec4af61b7adfddfaf3a68e1a74d5b2650ff859e8
HaYeonKim0807 <rlagkdus55@gmail.com>
2022-05-14 05:29:00 +0900

    first commit

---
+++ mustgo-r/first/models.py
@@ -0,0 +1,15 @@
+from email.headerregistry import Address
+from operator import truediv
+from tkinter.tix import MAX
+import turtle
+from django.db import models
+
+# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
---
+++ mustgo-r/first/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from first.models import Restaurant
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def list(request):
+    restaurants = Restaurant.objects.all()
+    paginator = Paginator(restaurants, 5)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context={
+        'restaurants' : items
+    }
+
+    return render(request, 'first/list.html', context)
