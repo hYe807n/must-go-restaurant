da44f6056188169110bc722a04ceb4c861a13148
HaYeonKim0807 <rlagkdus55@gmail.com>
2022-05-15 04:50:52 +0900

    second-commit_upload, delete, create...

---
+++ mustgo-r/first/forms.py
@@ -0,0 +1,22 @@
+from cProfile import label
+from django.forms import ModelForm
+from first.models import Restaurant
+from django.utils.translation import gettext_lazy as _
+
+class RestaurantForm(ModelForm):
+    class Meta:
+        model = Restaurant
+        fields = ['name', 'address']
+        labels = {
+            'name' : _('이름'),
+            'address' : _('주소'),
+        }
+        helt_texts = {
+            'name' : _('이름을 입력해주세요.'),
+            'address' : _('주소를 입력해주세요.'),
+        }
+        error_message = {
+            'name' : {
+                'max_length' : _('이름이 너무 깁니다. 30자 이하로 입력해주세요.'),
+            }
+        }
--- mustgo-r/first/views.py
+++ mustgo-r/first/views.py
@@ -1,18 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from first.models import Restaurant
 from django.core.paginator import Paginator
+from first.forms import RestaurantForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def list(request):
     restaurants = Restaurant.objects.all()
     paginator = Paginator(restaurants, 5)
-
+    lastpage = int(paginator.num_pages)-1
     page = request.GET.get('page')
     items = paginator.get_page(page)
 
     context={
-        'restaurants' : items
+        'restaurants' : items, 
+        'lastPage' : lastpage,
     }
 
     return render(request, 'first/list.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+        return HttpResponseRedirect('/first/list/')
+    form = RestaurantForm()
+    return render(request, 'first/create.html', {'form' : form})
+
+
+def update(request):
+    if request.method == "POST" and 'id' in request.POST :
+        #item = Restaurant.objects.get(pk=request.POST.get('id'))
+        item = get_object_or_404(Restaurant, pk = request.POST.get('id'))
+        form = RestaurantForm(request.POST, instance = item)
+        if form.is_valid():
+            item = form.save()
+    elif request.method == "GET" :
+        #item = Restaurant.objects.get(pk = request.GET.get('id'))
+        item = get_object_or_404(Restaurant, pk = request.GET.get('id'))
+        form = RestaurantForm(instance=item)
+        return render(request, 'first/update.html', {'form' : form})
+    return HttpResponseRedirect('/first/list/')
+
+
+def detail(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk = request.GET.get('id'))
+        return render(request, 'first/detail.html', {'item' : item})
+    return HttpResponseRedirect('/first/list/')
+
+
+def delete(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk = request.GET.get('id'))
+        item.delete()
+    return HttpResponseRedirect('/first/list/')
